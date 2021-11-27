36fd87e95413e0bcd9b990bfc6ad7107b516cc75
ToastyMod <toastymod@gmail.com>
2021-11-27 16:01:06 -0700

    add campfires

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 
 connect()
-.use(serveStatic("./web"))
+.use(serveStatic("./"))
 .listen(8080, () => console.log('Server running on 8080...'));
