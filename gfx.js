e79be608d4f614372957d7a012b1fb3440a10378
ToastyMod <toastymod@gmail.com>
2021-11-27 00:00:53 -0700

    added camera and movement

--- gfx.js
+++ gfx.js
@@ -1,3 +1,15 @@
+
+function clamp(min,max,val){
+  if(val < min) {
+    return min;
+  }else if(val > max) {
+    return max;
+  }else{ 
+    return val;
+  }
+
+}
+
 var two = new Two({
   fullscreen: true,
   autostart: true
@@ -5,18 +17,60 @@ var two = new Two({
 
 var stage = new Two.Group();
 
+player_pos = [0.0,0.0];
+m_anchor = [0.0,0.0]; 
+entities = {};
+delta_mult = 0.1;
+m_delta = [0.0,0.0];
+
 for (var i = 0; i < 100; i++) {
   var x = Math.random() * two.width * 2 - two.width;
   var y = Math.random() * two.height * 2 - two.height;
   var size = 50;
-  var shape = new Two.Circle(x, y, size);
-  shape.noStroke().fill = '#ccc';
-  stage.add(shape);
+  entities[i] = new Two.Circle(x, y, size);
+  entities[i].noStroke().fill = '#ccc';
+  stage.add(entities[i]);
 }
 
-shape.fill = 'red';
-shape.position.set(two.width / 2, two.height / 2);
+entities[99].fill = 'red';
+entities[99].position.set(two.width / 2, two.height / 2);
 
 two.add(stage);
+
+dragging = false;
+
+$("body").mousedown(function(e){
+  dragging = true;
+  m_anchor = [e.pageX, e.pageY];
+});
+
+$("body").mouseup(function(){
+  dragging = false;
+  m_delta = [0.0,0.0];
+});
+
+$("body").mouseleave(function(){
+  dragging = false;
+  m_delta = [0.0,0.0];
+});
+
+$("body").mousemove(function(e){
+  if(dragging) {
+    m_delta = [
+      clamp(-25,25,m_anchor[0]-e.pageX), 
+      clamp(-25,25,m_anchor[1]-e.pageY)
+    ];
+  }
+});
+
+two.bind('update', function(){
+  player_pos[0] -= m_delta[0] * delta_mult;
+  player_pos[1] -= m_delta[1] *delta_mult;
+  entities[99].position.set(player_pos[0] + (two.width / 2), player_pos[1] + (two.height / 2));
+
+  stage.translation.x += m_delta[0] * delta_mult;
+  stage.translation.y += m_delta[1] * delta_mult;
+
+});
 
 
